3d651d1ccaf21dafd3c3fa7536f554d779fee5e9
Sushil Kumar <sushil.18je0861@am.iitism.ac.in>
2020-11-20 15:22:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,55 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const posts = require('./initialData');
- 
- 
-// Set express APP
-const app = express();
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser");
+const port = 3000
+app.use(express.urlencoded());
 
-// Middleware 
-app.use(cors());
-app.use(bodyParser.json());
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
 
 
+app.use(bodyParser.urlencoded({ extended: false }))
 
-// Rate Limiting api calls
-let apiCalls = 0;
-let cachedTime;
-let cachedData;
-
-// Routes
-app.get('/api/posts', (req, res) =>{
-    apiCalls++;
-    if(!cachedTime || Date.now() - cachedTime > 30 * 1000){
-        let mx = (req.query.max && req.query.max <= 20)? req.query.max : 10;
-        cachedTime = Date.now();
-        cachedData = posts.slice(0, parseInt(mx))
-        apiCalls = 1;
-        res.status(200);
-        res.json(cachedData);
-    } else{
-        if(apiCalls > 5){
-            res.status(429);
-            res.json({message: "Exceed Number of API Calls"});
-        } else{
-            let mx = (req.query.max && req.query.max <= 20)? req.query.max : 10;
-            res.status(200);
-            res.json(cachedData.slice(0, Math.min(cachedData.length, parseInt(mx))));
-        }
-    }
-})
+app.use(bodyParser.json())
+// your code goes here
 
 
-app.get('*', (req, res) =>{
-    res.status(404);
-    res.send("PAGE NOT FOUND - 404");
-})
+app.listen(port, () => console.log(`App listening on port ${port}!`))
 
-
-// Listen At port
-app.listen(3000, () => {
-    console.log('Listening At Port 3000');
-    app.emit('appStarted');
-});
 module.exports = app;
